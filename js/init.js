e1e4eb7d2f795e3040f81fa9c45bb25f20809ed3
patinoroblerojulioashley <152238767+patinoroblerojulioashley@users.noreply.github.com>
2023-12-20 23:51:50 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,10 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyCbdvkbWQr0hBOyGSUvFAlaPqsmucmuQFg",
+  authDomain: "pra4japr.firebaseapp.com",
+  projectId: "pra4japr",
+  storageBucket: "pra4japr.appspot.com",
+  messagingSenderId: "771503551721",
+  appId: "1:771503551721:web:8a5b41dcf83ea0d5682c78"
 });
